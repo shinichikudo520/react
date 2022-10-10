11b299dd4b52768d00afb8388eaa54104532a104
shinichikudo520 <2098971607@qq.com>
2022-10-10 23:25:04 +0800

    install

--- 01_react-app/src/component/app/app.jsx
+++ 01_react-app/src/component/app/app.jsx
@@ -1,16 +1,43 @@
 import React,{Component} from 'react';
-
-import './app.css'
-import Search from '../search/search'
-import List from '../list/list'
-
+import './app.css';
+import CommentAdd from '../comment-add/comment-add'
+import CommentList from '../comment-list/comment-list'
 export default class App extends Component{
-
+    //初始化类组件的state属性
+    state = {
+        comments : [
+            {
+                userName:'Tom',
+                say:'React真不错！'
+            },
+            {
+                userName:'Jack',
+                say:'React太难了！'
+            }
+        ],
+    };
+    addComment = (comment) =>{
+        let {comments} = this.state;
+        comments.unshift(comment);
+        this.setState(comments);
+    }
+    handleDelete = (index) => {
+        let {comments} = this.state;
+        comments.splice(index,1);
+        this.setState(comments);
+    }
     render(){
         return (
-            <div id='App'>
-                <Search />
-                <List  />
+            <div className='app'>
+                <h1>请发表对React的评论</h1>
+                <div className='comment'>
+                    <div className='comment-add'>
+                        <CommentAdd addComment={this.addComment} />
+                    </div>
+                    <div className='comment-list'>
+                        <CommentList comments={this.state.comments} handleDelete={this.handleDelete} />
+                    </div>
+                </div>
             </div>
         );
     }
