260ec18ad4da41b9a0ca9e016b58aff86cb17a57
shinichikudo520 <2098971607@qq.com>
2022-10-20 18:38:08 +0800

    comment-demo

---
+++ 02_react-app/comment/src/components/comment-list/comment-list.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './commentList.css';
+
+export default class ComponentList extends React.Component {
+  constructor() {}
+  render() {
+    return (
+      <div className="col-md-8">
+        <h3 className="reply">评论回复：</h3>
+        <h2 style={{ display: 'none' }}>暂无评论，点击左侧添加评论！！！</h2>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <div className="handle">
+              <a href="www.baidu.com">删除</a>
+            </div>
+            <p className="user">
+              <span>xxx</span>
+              <span>说:</span>
+            </p>
+            <p className="centence">React不错!</p>
+          </li>
+          <li className="list-group-item">
+            <div className="handle">
+              <a href="www.baidu.com">删除</a>
+            </div>
+            <p className="user">
+              <span>yyy</span>
+              <span>说:</span>
+            </p>
+            <p className="centence">React有点难!</p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+ComponentList.propTypes = {
+  comments: PropTypes.array.isRequired,
+};
--- 02_react-app/comment/src/index.js
+++ 02_react-app/comment/src/index.js
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import {BrowserRouter} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App/>
-	</BrowserRouter>,
-	document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
