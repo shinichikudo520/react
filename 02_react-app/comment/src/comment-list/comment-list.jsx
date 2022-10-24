e88dac1c79c85b49fe9567509fd2b0dfaf337b52
unknown <jlc_0226@SZ-JLC.COM>
2022-10-24 10:31:54 +0800

    移动文件夹位置

--- 02_react-app/comment/src/comment-list/comment-list.jsx
+++ 02_react-app/comment/src/comment-list/comment-list.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './commentList.css';
+import "./commentList.css";
 
 export default class ComponentList extends React.Component {
-  constructor() {}
+  constructor() {
+    super();
+
+    this.state = {};
+  }
   render() {
     return (
       <div className="col-md-8">
         <h3 className="reply">评论回复：</h3>
-        <h2 style={{ display: 'none' }}>暂无评论，点击左侧添加评论！！！</h2>
+        <h2 style={{ display: "none" }}>暂无评论，点击左侧添加评论！！！</h2>
         <ul className="list-group">
           <li className="list-group-item">
             <div className="handle">
