98f0fd54ee997abd0eddd1496d26870e75d270da
unknown <jlc_0226@SZ-JLC.COM>
2022-10-24 11:38:25 +0800

    02_react_app/comment项目

--- 02_react-app/comment/src/comment-add/comment-add.jsx
+++ 02_react-app/comment/src/comment-add/comment-add.jsx
@@ -1,31 +1,76 @@
 import React from "react";
 
-export default class ComponentAdd extends React.Component{
-    render() {
-        return (
-            <div className="col-md-4">
-            <form className="form-horizontal">
-              <div className="form-group">
-                <label>用户名</label>
-                <input type="text" className="form-control" placeholder="用户名" />
-              </div>
-              <div className="form-group">
-                <label>评论内容</label>
-                <textarea
-                  className="form-control"
-                  rows="6"
-                  placeholder="评论内容"
-                ></textarea>
-              </div>
-              <div className="form-group">
-                <div className="col-sm-offset-2 col-sm-10">
-                  <button type="button" className="btn btn-default pull-right">
-                    提交
-                  </button>
-                </div>
-              </div>
-            </form>
+import PropTypes from "prop-types";
+export default class ComponentAdd extends React.Component {
+  static propTypes = {
+    addComment: PropTypes.func.isRequired,
+  };
+  state = {
+    userName: "",
+    content: "",
+  };
+  constructor(props) {
+    super(props);
+  }
+  // 箭头函数不需要在 constructor 进行绑定 this.add = this.add.bind(this)
+  add = () => {
+    const { userName, content } = this.state;
+    const { addComment } = this.props;
+    addComment(userName, content);
+
+    // 清空输入框
+    this.setState({
+      userName: "",
+      content: "",
+    });
+  };
+  changeUserName = (e) => {
+    const userName = e.target.value;
+    this.setState({ userName });
+  };
+  changeContent = (e) => {
+    const content = e.target.value;
+    this.setState({ content });
+  };
+
+  render() {
+    const { userName, content } = this.state;
+    return (
+      <div className="col-md-4">
+        <form className="form-horizontal">
+          <div className="form-group">
+            <label>用户名</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="用户名"
+              value={userName}
+              onChange={this.changeUserName}
+            />
           </div>
-        )
-    }
+          <div className="form-group">
+            <label>评论内容</label>
+            <textarea
+              className="form-control"
+              rows="6"
+              placeholder="评论内容"
+              value={content}
+              onChange={this.changeContent}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-offset-2 col-sm-10">
+              <button
+                type="button"
+                className="btn btn-default pull-right"
+                onClick={this.add}
+              >
+                提交
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
--- 02_react-app/comment/src/comment-list/comment-list.jsx
+++ 02_react-app/comment/src/comment-list/comment-list.jsx
@@ -2,45 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./commentList.css";
+import ComponentItem from "../comment-item/comment-item";
 
 export default class ComponentList extends React.Component {
-  constructor() {
-    super();
+  // 给组件类指定属性
+  static propTypes = {
+    comments: PropTypes.array.isRequired,
+    removeComment: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
 
     this.state = {};
   }
   render() {
+    const { comments } = this.props;
+    const display = comments.length === 0 ? "block" : "none";
     return (
       <div className="col-md-8">
         <h3 className="reply">评论回复：</h3>
-        <h2 style={{ display: "none" }}>暂无评论，点击左侧添加评论！！！</h2>
+        <h2 style={{ display: display }}>暂无评论，点击左侧添加评论！！！</h2>
         <ul className="list-group">
-          <li className="list-group-item">
-            <div className="handle">
-              <a href="www.baidu.com">删除</a>
-            </div>
-            <p className="user">
-              <span>xxx</span>
-              <span>说:</span>
-            </p>
-            <p className="centence">React不错!</p>
-          </li>
-          <li className="list-group-item">
-            <div className="handle">
-              <a href="www.baidu.com">删除</a>
-            </div>
-            <p className="user">
-              <span>yyy</span>
-              <span>说:</span>
-            </p>
-            <p className="centence">React有点难!</p>
-          </li>
+          {comments.map((comment, index) => (
+            <ComponentItem
+              comment={comment}
+              key={index}
+              index={index}
+              removeComment={this.props.removeComment}
+            />
+          ))}
         </ul>
       </div>
     );
   }
 }
-
-ComponentList.propTypes = {
-  comments: PropTypes.array.isRequired,
-};
