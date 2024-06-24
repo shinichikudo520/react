eb4fd788dbedade2a15d254a33f95d4f41692dd6
shinichikudo520 <2098971607@qq.com>
2024-06-24 19:39:23 +0800

    调整

--- hooks.2024/my-app/src/App.tsx
+++ hooks.2024/my-app/src/App.tsx
@@ -11,18 +11,25 @@ function App() {
   return (
     <div className="App">
       <h1>React Hooks</h1>
+      <h2>一、UseState 组件</h2>
       <UseState />
       <hr />
+      <h2>二、UseEffect 组件</h2>
       <UseEffect />
       <hr />
+      <h2>三、UseMemo 组件</h2>
       <UseMemo />
       <hr />
+      <h2>四、UseCallback 组件</h2>
       <UseCallback />
       <hr />
+      <h2>五、UseRef 组件</h2>
       <UseRef />
       <hr />
+      <h2>六、UseContext 组件</h2>
       <UseContext />
       <hr />
+      <h2>七、UseReducer 组件</h2>
       <UseReducer />
     </div>
   );
